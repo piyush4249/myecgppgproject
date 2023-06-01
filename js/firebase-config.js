936e779eb42fdb0d78fd195e2cf222693ef52f68
piyush4249 <134853929+piyush4249@users.noreply.github.com>
2023-06-01 15:42:33 +0530

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Your Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCOQetfF1-4CH9lMecbVfTSn87yPPKxWzQ",
+    authDomain: "august-outlet-377807.firebaseapp.com",
+    projectId: "august-outlet-377807",
+    storageBucket: "august-outlet-377807.appspot.com",
+    messagingSenderId: "571118008986",
+    appId: "1:571118008986:web:a5e3df52c93a1ff8eefd23"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
